a44de99449ba26898626f41de347fcaffcb2e8a7
Julio Bisaia Rodrigues <juliobisaia@hotmail.com>
2021-05-05 23:02:32 -0300

    Update desafio03_somaDosNumeros.py

--- python/desafios/desafio03_somaDosNumeros.py
+++ python/desafios/desafio03_somaDosNumeros.py
@@ -1,3 +1,9 @@
-num1 = int(input('Digite um numero '));
-num2 = int(input('Digite outro numero '));
-print('A soma de',num1, '+', num2, 'é:',num1+num2);
+num1 = int(input('Digite um numero '))
+num2 = int(input('Digite outro numero '))
+print('A soma de', num1, '+', num2, 'é:', num1+num2)
+
+if(n1.isalnum()){
+    print('É numero')
+}else{
+    print('Não é numero')
+}
