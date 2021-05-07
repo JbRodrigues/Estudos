70ad13762af4e6a4d5dd0b4ca688acc8d1f2ffae
Julio Bisaia Rodrigues <juliobisaia@hotmail.com>
2021-05-06 22:27:25 -0300

    Update Dados.py

--- python/testes/Dados.py
+++ python/testes/Dados.py
@@ -1,13 +1,3 @@
-import random
-
-dado = random.randrange(1,6)
-
-num = int(input('Advinhe o número'))
-aux = 1;
 
 
-while aux != 0:
-    num = int(input('Tente Outra vez !!! '))
-    if num == dado:
-        print('Parabens, você Acertou!!!')
-        aux = 0;
+print('testanto o bagui')
